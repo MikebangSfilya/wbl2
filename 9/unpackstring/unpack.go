2308c39de875ca9f0a95c8279e77514deefbfb6d
Sfilya <Typegoot@gmail.com>
2026-03-03 23:15:36 +0300

    2.9

---
+++ 9/unpackstring/unpack.go
@@ -0,0 +1,32 @@
+package unpackstring
+
+import (
+	"errors"
+	"strings"
+	"unicode"
+)
+
+var errDigit = errors.New("ERROR: first argument is digit")
+
+func Unpack(s string) (string, error) {
+	sb := strings.Builder{}
+	sb.Grow(len(s))
+	var prev rune
+	for _, v := range s {
+
+		if !unicode.IsDigit(v) {
+			prev = v
+			sb.WriteString(string(prev))
+		} else {
+			if prev == 0 {
+				return "", errDigit
+			}
+			n := int(v-'0') - 1
+			for range n {
+				sb.WriteString(string(prev))
+			}
+
+		}
+	}
+	return sb.String(), nil
+}
---
+++ 9/unpackstring/unpack_test.go
@@ -0,0 +1,50 @@
+package unpackstring
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestUnpack(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+		wantErr  error
+	}{
+		{
+			name:     "test_case1",
+			input:    "a4bc2d5e",
+			expected: "aaaabccddddde",
+		},
+		{
+			name:     "test_case2",
+			input:    "abcd",
+			expected: "abcd",
+		},
+		{
+			name:    "test_case3",
+			input:   "45",
+			wantErr: errDigit,
+		},
+		{
+			name:     "",
+			input:    "",
+			expected: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Unpack(tt.input)
+			if tt.wantErr != nil {
+				if !errors.Is(err, tt.wantErr) {
+					t.Errorf("expected %v, got %v", tt.wantErr, err)
+				}
+			} else {
+				if got != tt.expected {
+					t.Fatalf("expected %s, got %s", tt.expected, got)
+				}
+			}
+		})
+	}
+}
