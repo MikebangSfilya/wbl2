3e1c9605b0f7e92cdf0976ef4d0318e2294e72cc
Sfilya <Typegoot@gmail.com>
2026-03-05 22:12:13 +0300

    reverse flag, sorter struct

--- 10/main.go
+++ 10/main.go
@@ -2,52 +2,68 @@ package main
 
 import (
 	"cmp"
-	"flag"
 	"fmt"
 	"log/slog"
 	"os"
 	"slices"
 	"strings"
+
+	flag "github.com/spf13/pflag"
 )
 
-type Sort struct {
-	dataBlocks [][]string
+type Sorter struct {
+	Lines   []string
+	Column  int
+	Reverse bool
+	Num     bool
+	Uniq    bool
 }
 
-func (s *Sort) Sort(input []byte, column int) {
-	column -= 1
-	if column < 0 {
-		column = 0
-	}
-
+func (s *Sorter) prepare(input []byte) {
 	str := strings.TrimSpace(string(input))
-
 	stringsInput := strings.Split(str, "\n")
+	s.Lines = stringsInput
+	slog.Debug("s.data", "[]string", stringsInput)
+}
 
-	for _, v := range stringsInput {
-		fields := strings.Fields(v)
-		if len(fields) > 0 {
-			s.dataBlocks = append(s.dataBlocks, fields)
-		}
-	}
-	slog.Debug("s.data", "[]string", s.dataBlocks)
+func (s *Sorter) sort() {
+	slices.SortFunc(s.Lines, func(a, b string) int {
+		aFiied := strings.Fields(a)
+		bField := strings.Fields(b)
+		// slog.Debug("fields", "aField", aFiied, "bField", bField)
 
-	slices.SortStableFunc(s.dataBlocks, func(a, b []string) int {
-		if column >= len(a) || column >= (len(b)) {
+		if s.Column >= len(aFiied) || s.Column >= len(bField) {
 			return 0
 		}
 
-		return cmp.Compare(a[column], b[column])
+		if s.Reverse {
+			return cmp.Compare(b[s.Column], a[s.Column])
+		}
+
+		return cmp.Compare(a[s.Column], b[s.Column])
 	})
-	for _, v := range s.dataBlocks {
-		fmt.Println(strings.Join(v, " "))
+}
+
+func (s *Sorter) Sort(input []byte) {
+	s.Column -= 1
+	if s.Column < 0 {
+		s.Column = 0
 	}
+
+	s.prepare(input)
+	s.sort()
+	for _, v := range s.Lines {
+		fmt.Println(v)
+	}
+
 }
 
 func main() {
-	setupLogger(slog.LevelInfo)
+	setupLogger(slog.LevelDebug)
 
-	columnPtr := flag.Int("k", 1, "Column number to sort by (1-based)")
+	columnPtr := flag.IntP("column", "k", 1, "Column number to sort by")
+	reversePtr := flag.BoolP("reverse", "r", false, "Reverse sort")
+	uniquePtr := flag.BoolP("unique", "u", false, "Unique lines only")
 
 	flag.Parse()
 
@@ -56,8 +72,13 @@ func main() {
 		slog.Error("cant read file", "error", err)
 		os.Exit(1)
 	}
-	s := Sort{}
-	s.Sort(data, *columnPtr)
+	s := Sorter{
+		Column:  *columnPtr,
+		Reverse: *reversePtr,
+		Uniq:    *uniquePtr,
+	}
+
+	s.Sort(data)
 }
 
 func setupLogger(level slog.Level) {
@@ -65,7 +86,7 @@ func setupLogger(level slog.Level) {
 		Level: level,
 	}
 
-	handler := slog.NewJSONHandler(os.Stdout, opts)
+	handler := slog.NewTextHandler(os.Stdout, opts)
 	logger := slog.New(handler)
 
 	slog.SetDefault(logger)
