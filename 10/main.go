2f5311566a87d1afe543a1f1feb5146c5e64dbbf
Sfilya <Typegoot@gmail.com>
2026-03-06 10:19:44 +0300

    final

--- 10/main.go
+++ 10/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"cmp"
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
 	"slices"
+	"strconv"
 	"strings"
 
 	flag "github.com/spf13/pflag"
@@ -20,11 +23,27 @@ type Sorter struct {
 	Uniq    bool
 }
 
-func (s *Sorter) prepare(input []byte) {
-	str := strings.TrimSpace(string(input))
-	stringsInput := strings.Split(str, "\n")
-	s.Lines = stringsInput
-	slog.Debug("s.data", "[]string", stringsInput)
+func (s *Sorter) parseFloat(v string) float64 {
+	if v == "" {
+		return 0
+	}
+	val, err := strconv.ParseFloat(v, 64)
+	if err != nil {
+		return 0
+	}
+	return val
+}
+
+func (s *Sorter) Read(r io.Reader) error {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		s.Lines = append(s.Lines, line)
+	}
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("error reading input: %w", err)
+	}
+	return nil
 }
 
 func (s *Sorter) unique() {
@@ -37,10 +56,7 @@ func (s *Sorter) unique() {
 		}
 	}
 
-	s.Lines = s.Lines[:0]
-	for _, v := range result {
-		s.Lines = append(s.Lines, v)
-	}
+	s.Lines = append(s.Lines[:0], result...)
 }
 
 func (s *Sorter) sort() {
@@ -56,6 +72,18 @@ func (s *Sorter) sort() {
 			bVal = bField[s.Column]
 		}
 
+		if s.Num {
+			aFloat := s.parseFloat(aVal)
+			bFloat := s.parseFloat(bVal)
+
+			if s.Reverse {
+				return cmp.Compare(bFloat, aFloat)
+			}
+
+			return cmp.Compare(aFloat, bFloat)
+
+		}
+
 		if s.Reverse {
 			return cmp.Compare(bVal, aVal)
 		}
@@ -64,13 +92,12 @@ func (s *Sorter) sort() {
 	})
 }
 
-func (s *Sorter) Sort(input []byte) {
+func (s *Sorter) Sort() {
 	s.Column -= 1
 	if s.Column < 0 {
 		s.Column = 0
 	}
 
-	s.prepare(input)
 	if s.Uniq {
 		s.unique()
 	}
@@ -88,21 +115,40 @@ func main() {
 	columnPtr := flag.IntP("column", "k", 1, "Column number to sort by")
 	reversePtr := flag.BoolP("reverse", "r", false, "Reverse sort")
 	uniquePtr := flag.BoolP("unique", "u", false, "Unique lines only")
+	numPtr := flag.BoolP("numeric", "n", false, "Compare according to string numerical value")
 
 	flag.Parse()
 
-	data, err := os.ReadFile("file.txt")
-	if err != nil {
-		slog.Error("cant read file", "error", err)
-		os.Exit(1)
+	var input io.Reader
+	args := flag.Args()
+
+	if len(args) == 0 {
+		input = os.Stdin
+	} else {
+		file, err := os.Open(args[0])
+		if err != nil {
+			slog.Error("cant open file", "error", err)
+			os.Exit(1)
+		}
+		defer func() {
+			_ = file.Close()
+		}()
+		input = file
 	}
+
 	s := Sorter{
 		Column:  *columnPtr,
 		Reverse: *reversePtr,
 		Uniq:    *uniquePtr,
+		Num:     *numPtr,
 	}
 
-	s.Sort(data)
+	if err := s.Read(input); err != nil {
+		slog.Error("read error", "error", err)
+		os.Exit(1)
+	}
+
+	s.Sort()
 }
 
 func setupLogger(level slog.Level) {
